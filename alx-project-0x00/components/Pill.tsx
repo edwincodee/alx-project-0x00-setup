b398f0017b56c7ae24f4b4fa4b9778c79d37ac6a
edwincodee <edwiinnemmanuell@gmail.com>
2025-07-16 15:58:29 +0100

    Typing your functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,6 +1,6 @@
 import { PillProps } from "@/interfaces";
 
-const Pill: React.FC = ({tittle}) => {
+const Pill: React.FC<PillProps> = ({tittle}) => {
     return (
         <div className=" flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
             <p className="text-sm">{tittle}</p>
